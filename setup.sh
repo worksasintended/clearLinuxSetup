cc1c57dab6233d1a6e9749b624ca722c00ef6b34
Marc Marschall <marc@marschalls.net>
2019-01-30 13:05:54 +0000

    minor work

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #install bundles
-
+swupd bundle-add vim hpc-utils linux-dev dev-utils dev-utils-dev findutils make openssh-server
 #network
 mkdir -p /etc/systemd/network/
 cp 70-static.network/etc/systemd/network/70-static.network
 cp 71-inf.network/etc/systemd/network/71-inf.network
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
-systemctl restart systemd-networkd
-systemctl restart systemd-resolved
-
+echo "make sure to add an ssh user"
